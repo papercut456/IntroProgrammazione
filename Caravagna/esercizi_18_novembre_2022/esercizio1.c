c60b4767665917eef2ac5a3ff495231e4cb45ebb
papercut456 <115028008+papercut456@users.noreply.github.com>
2022-11-18 11:14:30 +0000

    Updated Directories ed esercizi del 18 novembre 2022

---
+++ Caravagna/esercizi_18_novembre_2022/esercizio1.c
@@ -0,0 +1,24 @@
+#include   <stdio.h>
+#include   <stdlib.h>
+int n;
+esercizio1(float array[]){
+printf("Definisci Dimensione Array ?");
+    scanf("%d", &n);
+    
+    float array[n];
+    for(i=0, i<n,i++){
+        if n%2 == 0
+        array[i]=n;
+        n++;
+        else n++; }
+       
+}
+
+
+void main()
+{
+    float test1[];
+    esercizio1(test1[];);
+    return 0;
+
+    }
